cdf0730e327f7d8524bbb182fde5f699e1f2643a
gsuyemoto <gsuyemoto@gmail.com>
2023-08-12 14:53:13 -0700

    Figured out how to get ONVIF info from device. After device discovery, which uses a totally different protocol (ws-discovery and SOAP UDP), you need to switch to HTTP calls to the xaddrs provided in device discovery. This calls device management API. These calls do not use raw udpsocket and instead use the reqwest crate.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use anyhow::Result;
+use bytes::Bytes;
+use reqwest::{Client, RequestBuilder};
 use std::io::BufReader;
 use std::net::{SocketAddr, UdpSocket};
 use xml::reader::{EventReader, XmlEvent};
@@ -6,25 +9,32 @@ use xml::reader::{EventReader, XmlEvent};
 enum Messages {
     Discovery,
     Capabilities,
+    DeviceInfo,
 }
 
 const BIND_TO_ANY_IP: &'static str = "0.0.0.0:0";
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let socket = UdpSocket::bind(BIND_TO_ANY_IP)?;
 
     //------------------- DISCOVER ALL ONVIF DEVICES
     //-------------------
 
-    let message = get_message(Messages::Discovery);
+    // discovery is using SOAP UDP based in ws-discovery
+    // and therefore just uses a raw udpsocket
+    let soap_message = get_message(Messages::Discovery);
 
     // Don't make these const as they are used only once
-    let multicast_ip = "239.255.255.250";
-    let multicast_port = 3702;
+    // and therefore after use we can drop from memory
+    let send_ip = "239.255.255.250";
+    let send_port = 3702;
 
-    let (socket_buffer, size_msg) =
-        send_and_get_response(&socket, multicast_ip, multicast_port, message);
+    let (socket_buffer, size_msg) = discover_devices(&socket, send_ip, send_port, soap_message);
 
+    // main thing we need here is the xaddrs
+    // which is an HTTP URL to which we call later
+    // for "device management"
     let xaddrs = parse_xaddrs(socket_buffer, size_msg);
 
     println!("XAddrs: {xaddrs}");
@@ -32,10 +42,36 @@ fn main() -> std::io::Result<()> {
     //------------------- GET DEVICE CAPABILITIES
     //-------------------
 
+    let soap_message = get_message(Messages::DeviceInfo);
+
+    // after discovery, the xaddrs in the reply from each device
+    // will reveal the url needed for device management
+    // here the communication switches to requests sent via
+    // HTTP, but still using SOAP
+    // we are going to use reqwest to create HTTP requests
+    let response_bytes = onvif_message(xaddrs, soap_message).await?;
+    let response = String::from_utf8_lossy(response_bytes.as_ref());
+
+    println!("Received response: {}", response);
+
     Ok(())
 }
 
-fn send_and_get_response(
+async fn onvif_message(device_url: String, soap_msg: String) -> Result<Bytes> {
+    let client = Client::new();
+    let request: RequestBuilder = client
+        .post(device_url)
+        .header("Content-Type", "application/soap+xml; charset=utf-8")
+        .body(soap_msg);
+
+    // Send the HTTP request and receive the response
+    let response = request.send().await?;
+    let response_bytes = response.bytes().await?;
+
+    Ok(response_bytes)
+}
+
+fn discover_devices(
     socket: &UdpSocket,
     send_ip: &str,
     send_port: u16,
@@ -65,10 +101,6 @@ fn send_and_get_response(
         }
         Err(e) => eprintln!("Error receiving {e}"),
     }
-
-    // show raw XML message for testing which includes inner
-    // let response = String::from_utf8_lossy(&socket_buffer[..size]);
-    // println!("Received response: {}", response);
 
     (socket_buffer, size_msg)
 }
@@ -134,55 +166,38 @@ fn get_message(msg_type: Messages) -> String {
             </s:Envelope>"#
         ),
 
+        // Messages::Capabilities => format!(
+        //     r#"<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope"
+        //                  xmlns:tds="http://www.onvif.org/ver10/device/wsdl">
+        //        <Header/>
+        //        <Body>
+        //            <tds:GetCapabilities>
+        //                <tds:Category>All</tds:Category>
+        //            </tds:GetCapabilities>
+        //        </Body>
+        //     </Envelope>"#
+        // ),
         Messages::Capabilities => format!(
-            r#"<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope"
+            r#"
+            <Envelope xmlns="http://www.w3.org/2003/05/soap-envelope" xmlns:trt="http://www.onvif.org/ver10/media/wsdl">
+                <Header/>
+                <Body>
+                    <trt:GetCapabilities>
+                        <trt:Category>Media</trt:Category>
+                    </trt:GetCapabilities>
+                </Body>
+            </Envelope>
+        "#
+        ),
+        Messages::DeviceInfo => format!(
+            r#"
+            <s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope"
                          xmlns:tds="http://www.onvif.org/ver10/device/wsdl">
-               <Header/>
-               <Body>
-                   <tds:GetCapabilities>
-                       <tds:Category>All</tds:Category>
-                   </tds:GetCapabilities>
-               </Body>
-            </Envelope>"#
+                <s:Body>
+                    <tds:GetDeviceInformation/>
+                </s:Body>
+            </s:Envelope>
+        "#
         ),
     }
-}
-
-fn get_capabilities() {
-    // // Send the SOAP message over UDP
-    // socket.send_to(message_capabilities.as_bytes(), destination)?;
-
-    // println!("Sent capabilties message");
-
-    // // Receive response
-    // let mut socket_buffer = [0; 4096];
-    // let (size, _) = socket.recv_from(&mut socket_buffer)?;
-
-    // println!("Received message: {size}");
-
-    // let mut depth = 0;
-    // let buffer = BufReader::new(&socket_buffer[..size]);
-    // let parser = EventReader::new(buffer);
-
-    // for e in parser {
-    //     match e {
-    //         Ok(XmlEvent::StartElement { name, .. }) => {
-    //             // println!("{:spaces$}+{name}", "", spaces = depth * 2);
-    //             depth += 1;
-    //         }
-    //         Ok(XmlEvent::EndElement { name }) => {
-    //             depth -= 1;
-    //             // println!("{:spaces$}-{name}", "", spaces = depth * 2);
-    //         }
-    //         Ok(XmlEvent::Characters(chars)) => {
-    //             println!("{chars}");
-    //         }
-    //         Err(e) => {
-    //             eprintln!("Error: {e}");
-    //             break;
-    //         }
-    //         // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
-    //         _ => {}
-    //     }
-    // }
 }
